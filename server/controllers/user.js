9aba0f50b9de7d93be234906fe0ed1ca96996115
alejandramantillac <alejamantillac@gmail.com>
2021-12-14 18:16:26 -0500

    Commit

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const bcrypt = require('bcrypt-nodejs');
 const jwt = require('../services/jwt');
 const User = require('../models/user');
@@ -98,9 +100,62 @@ function getUsersActive(req, res) {
   });
 }
 
+function uploadAvatar(req, res) {
+  const params = req.params;
+
+  User.findById({ _id: params.id }, (err, userData) => {
+    if (err) {
+      res.status(500).send({ message: "Error del servidor." });
+    } else {
+      if (!userData) {
+        res.status(404).send({ message: "Nose ha encontrado ningun usuario." });
+      } else {
+        let user = userData;
+
+        if (req.files) {
+          let filePath = req.files.avatar.path;
+          let fileSplit = filePath.split("/");
+          let fileName = fileSplit[2];
+
+          let extSplit = fileName.split(".");
+          let fileExt = extSplit[1];
+
+          if (fileExt !== "png" && fileExt !== "jpg") {
+            res.status(400).send({
+              message:
+                "La extension de la imagen no es valida. (Extensiones permitidas: .png y .jpg)"
+            });
+          } else {
+            user.avatar = fileName;
+            User.findByIdAndUpdate(
+              { _id: params.id },
+              user,
+              (err, userResult) => {
+                if (err) {
+                  res.status(500).send({ message: "Error del servidor." });
+                } else {
+                  if (!userResult) {
+                    res
+                      .status(404)
+                      .send({ message: "No se ha encontrado ningun usuario." });
+                  } else {
+                    res.status(200).send({ avatarName: fileName });
+                  }
+                }
+              }
+            );
+          }
+        }
+      }
+    }
+  });
+}
+
+
 module.exports = {
     signUp,
     signIn,
     getUsers,
-    getUsersActive
+    getUsersActive,
+    uploadAvatar
 };
--- server/routers/user.js
+++ server/routers/user.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const UserController = require('../controllers/user');
-const md_auth = require('../middlewares/authenticated');
+const multipart = require('connect-multiparty');
 
+const md_auth = require("../middlewares/authenticated");
+const md_upload_avatar = multipart({ uploadDir: "./uploads/avatar" });
 
 const api = express.Router();
 
@@ -10,5 +12,9 @@ api.post('/sign-up', UserController.signUp);
 api.post('/sign-in', UserController.signIn);
 api.get('/users', [md_auth.ensureAuth], UserController.getUsers);
 api.get('/users-active', [md_auth.ensureAuth], UserController.getUsersActive);
-
+api.put(
+    "/upload-avatar/:id",
+    [md_auth.ensureAuth, md_upload_avatar],
+    UserController.uploadAvatar
+  );
 module.exports = api;
